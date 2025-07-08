fd059732ce2c0ac99cdf38d4c2960326c54fd7a1
Erenriquezp <erenriquezp@uce.edu.ec>
2025-07-08 12:51:33 -0500

    initialize project structure with multiple microservices and configuration files

---
+++ safeplace-service/src/main/java/com/safemap/safeplaceservice/SafeplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.safemap.safeplaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeplaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeplaceServiceApplication.class, args);
+    }
+
+}
---
+++ safeplace-service/src/test/java/com/safemap/safeplaceservice/SafeplaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safemap.safeplaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeplaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
